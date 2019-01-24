63266ae8a66073ba3269b87b4fa929b5656ee696
Lexx <Lexxiecole@gmail.com>
2019-01-23 20:27:01 -0500

    omg

---
+++ components/firstPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PageDiv = () => (
+  <div>
+    <h1 id = "smile">Yo Click on mmeee</h1>
+    <h2 id = "frown">It would make me so happy</h2>
+    <ul>
+      <li id = "rosie">Sweet Rosalie</li>
+      <li id = "percy">Percy Boi</li>
+      <li id = "boomy">Boomy</li>
+    </ul>
+  </div>
+);
+
+
+
+
+
+
+
+
+export default HelloDiv;
